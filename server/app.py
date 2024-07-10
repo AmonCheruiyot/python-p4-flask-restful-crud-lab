e06a9f306177339f3caf7576dbed9eee53ea445d
Amon <kiprotichamon03@gmail.com>
2024-07-10 22:01:56 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -42,7 +42,6 @@ api.add_resource(Plants, '/plants')
 
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
@@ -66,7 +65,6 @@ class PlantByID(Resource):
         db.session.commit()
 
         return make_response('', 204)
-
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
